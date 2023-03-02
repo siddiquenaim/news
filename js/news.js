4bf4100f0aa549f6213ea1430764d864c34da2ec
siddiquenaim <naim.niter.tex06@gmail.com>
2023-03-02 15:51:33 +0600

    added modal

--- js/news.js
+++ js/news.js
@@ -19,10 +19,106 @@ const displayCategories = (data) => {
 const categoryNews = (categoryId, categoryName) => {
   fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
     .then((res) => res.json())
-    .then((data) => displayAll(data, categoryName));
+    .then((data) => displayAll(data.data, categoryName));
 };
 
 const displayAll = (data, categoryName) => {
-  document.getElementById("news-count").innerText = data.data.length;
+  document.getElementById("news-count").innerText = data.length;
   document.getElementById("category-name").innerText = categoryName;
+  const allNews = document.getElementById("all-news");
+  allNews.innerHTML = "";
+  // news container section
+  data.forEach((singleNews) => {
+    console.log(singleNews);
+    const { image_url, details, title, author, total_view, _id } = singleNews;
+    allNews.innerHTML += `
+    <div class="card mb-3 container my-4">
+    <div class="row g-0">
+    <div class="col-md-4">
+        <img src="${image_url}" class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8 d-flex flex-column">
+        <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">
+            ${details.slice(0, 200)}...
+        </p>
+        </div>
+        <div class="card-footer border-0 bg-body d-flex justify-content-between">
+         <div class="d-flex align-items-center">
+         <div>
+                <img style="" src="${
+                  author.img
+                }" class="img-fluid rounded-circle me-3" alt="..." height='40' width='40' />
+            </div>
+            <div>
+                <p class="m-0 p-0">${author.name}</p>
+                <p class="m-0 p-0">${author.published_date}</p>
+            </div>
+         </div>
+         <div class="d-flex align-items-center"><i class="fa-solid fa-eye"></i> <p class="m-0 ms-2 p-0">${total_view}</p></div>
+         <div><i class="fa-regular fa-star"></i> </div>
+         <div><i onclick="getDetail('${_id}')" type="button"
+         data-bs-toggle="modal"
+         data-bs-target="#exampleModal" class="fa-solid fa-arrow-right"></i></div>        
+        </div>
+    </div>
+    </div>
+</div>
+    `;
+  });
 };
+
+// show news detail
+
+const getDetail = (news_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDetail(data.data[0]));
+};
+
+const displayDetail = (newsDetail) => {
+  const modalBody = document.getElementById("modalContent");
+  const { image_url, details, title, author, total_view, _id } = newsDetail;
+  modalBody.innerHTML = `
+  <div class="card mb-3 container my-4">
+  <div class="row g-0">
+  <div class="col-md-12">
+      <img src="${image_url}" class="img-fluid rounded" alt="..." />
+  </div>
+  <div class="">
+      <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text">
+          ${details}
+      </p>
+      </div>
+      <div class="card-footer border-0 bg-body d-flex justify-content-between">
+       <div class="d-flex align-items-center">
+       <div>
+              <img style="" src="${author.img}" class="img-fluid rounded-circle me-3" alt="..." height='40' width='40' />
+          </div>
+          <div>
+              <p class="m-0 p-0">${author.name}</p>
+              <p class="m-0 p-0">${author.published_date}</p>
+          </div>
+       </div>
+       <div class="d-flex align-items-center"><i class="fa-solid fa-eye"></i> <p class="m-0 ms-2 p-0">${total_view}</p></div>
+       <div><i class="fa-regular fa-star"></i> </div>
+  </div>
+  <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              </div>
+  </div>
+</div>
+  `;
+};
+
+categoryNews();
